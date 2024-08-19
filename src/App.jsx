e027e8b4462bc7bb54bd32bee5b6f090d7bece68
Eliz Erdim <elizerdim@gmail.com>
2024-08-19 14:43:49 +0300

    Add fetch method

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import { useEffect } from "react";
+
 function App() {
-  return (
-    <>
-      App
-    </>
-  )
+  useEffect(() => {
+    async function getFlights() {
+      const url = "https://api.schiphol.nl/public-flights";
+      try {
+        const res = await fetch(url, {
+          headers: {
+            "Content-Type": "application/json",
+            "app_id": "3bfc84de",
+            "app_key": "73ba042c7b8c776652f970d8fa83ae25",
+            "ResourceVersion": "v4"
+          },
+        });
+        const data = await res.json();
+        console.log(data);
+      } catch (err) {
+        console.log("Failed to fetch data: ", err.message);
+      }
+    }
+
+    getFlights();
+  }, []);
+
+  return <>App</>;
 }
 
-export default App
+export default App;
